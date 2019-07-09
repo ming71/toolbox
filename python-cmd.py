d301b6f222e59f1a7078d580e03c4df0f8086333
ming71 <43720092+ming71@users.noreply.github.com>
2019-07-09 21:20:48 +0800

    Update python-cmd.py

--- python-cmd.py
+++ python-cmd.py
@@ -7,6 +7,8 @@ cmd3 = 'cd /py && python '
 os.system(cmd2)
 
 # ---------带变量传入-------------
-# 使用格式化方法format(想了好久，发现这样可行！)
-path = '/py/pic'
-os.system('cd {} && ls'.format(path) )
+# 使用格式化方法format(想了好久，发现这样可行！)将一个文件目录下的文件移到另一个目录
+xml_src = r'/py/R2CNN-tensorflow/data/VOCdevkit/VOCdevkit_train/clip/aug/xml_src'
+xml_dst = r'/py/R2CNN-tensorflow/data/VOCdevkit/VOCdevkit_train/clip/aug/xml_dst'
+
+os.system('cp -r {} {}'.format(xml_src+'/* ',xml_dst))  # 注意src需要加'/*'
