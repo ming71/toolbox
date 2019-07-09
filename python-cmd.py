94f6eab4c11c66a3a5c4d6611f41b3eadd0d77fc
ming71 <43720092+ming71@users.noreply.github.com>
2019-07-09 21:12:39 +0800

    Update python-cmd.py

--- python-cmd.py
+++ python-cmd.py
@@ -1,6 +1,12 @@
 import os 
 
+# ---------简单例子-------------
 cmd1 = 'ls'
 cmd2 = 'cd /py/pic && ls'
 cmd3 = 'cd /py && python '
 os.system(cmd2)
+
+# ---------带变量传入-------------
+# 使用格式化方法format(想了好久，发现这样可行！)
+path = '/py/pic'
+os.system('cd {} && ls'.format(path) )
