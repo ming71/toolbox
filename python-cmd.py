00d53c7cfce088f174ddd909ca4c3583715728ce
ming71 <43720092+ming71@users.noreply.github.com>
2019-07-12 16:25:29 +0800

    Update python-cmd.py

--- python-cmd.py
+++ python-cmd.py
@@ -1,5 +1,11 @@
 import os 
 
+# 注意： os.system的命令不会保留，所以必须一次执行完全部命令。如第一句是cd xxx，第二句python xxx.py，必须写在一句话里，否则第一句执行完程序不会真的进入文件夹等待第二句
+# 返回值正常是0，可用于检测
+# 如下实例：
+# status=os.system('cd  ~/ev/yolo-master/keras-yolo3 && python yolo.py')
+
+
 # ---------简单例子-------------
 cmd1 = 'ls'
 cmd2 = 'cd /py/pic && ls'
@@ -12,3 +18,5 @@ xml_src = r'/py/R2CNN-tensorflow/data/VOCdevkit/VOCdevkit_train/clip/aug/xml_src
 xml_dst = r'/py/R2CNN-tensorflow/data/VOCdevkit/VOCdevkit_train/clip/aug/xml_dst'
 
 os.system('cp -r {} {}'.format(xml_src+'/* ',xml_dst))  # 注意src需要加'/*'
+
+
