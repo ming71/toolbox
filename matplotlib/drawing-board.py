778747dbb171df2d87a261e44d1c222046dca914
ming71 <chaser.ming@gmail.com>
2019-10-21 10:41:36 +0800

    relayout

--- matplotlib/drawing-board.py
+++ matplotlib/drawing-board.py
@@ -15,29 +15,33 @@ def abs_path(parentpath,subpath):
     return out
 
 if __name__ == "__main__":
-    path = r'/py/yolov3/cam/rlt'
-    figs = (3,3)    # 设置行列
+    path = r'/py/matrix-yolo/cam'
+    figs = (5,3)    # 设置行列数
+    row_name = 'fp'   # 设置行列图像编号名(如fp,anchor)
+    col_name = 'anchor'
     
-    plt.figure(figsize=(10, 10))
+    
+    plt.figure(figsize=(100, 100))
     # 行列设置
-    fig1_rows_1 = ['cls_0_0_(1).jpg','cls_0_1_(1).jpg','cls_0_2_(1).jpg']
-    fig1_rows_2 = ['cls_1_0_(1).jpg','cls_1_1_(1).jpg','cls_1_2_(1).jpg']
-    fig1_rows_3 = ['cls_2_0_(1).jpg','cls_2_1_(1).jpg','cls_2_2_(1).jpg']
-    # fig1_rows_1 = ['bam_0_0_cls2.jpg','bam_0_1_cls2.jpg','bam_0_2_cls2.jpg']
-    # fig1_rows_2 = ['bam_1_0_cls2.jpg','bam_1_1_cls2.jpg','bam_1_2_cls2.jpg']
-    # fig1_rows_3 = ['bam_2_0_cls2.jpg','bam_2_1_cls2.jpg','bam_2_2_cls2.jpg']
+    fig1_rows_1 = ['10.jpg','11.jpg','12.jpg']
+    fig1_rows_2 = ['70.jpg','71.jpg','72.jpg']
+    fig1_rows_3 = ['80.jpg','81.jpg','82.jpg']
+    fig1_rows_4 = ['90.jpg','91.jpg','92.jpg']
+    fig1_rows_5 = ['100.jpg','101.jpg','102.jpg']
 
-    subpath = [fig1_rows_1, fig1_rows_2, fig1_rows_3]
+
+    subpath = [fig1_rows_1, fig1_rows_2, fig1_rows_3,fig1_rows_4,fig1_rows_5]
     outpath = abs_path(path,subpath)
 
     cnt = 0
     for r,row in enumerate(outpath):
         for c,img_path in enumerate(row):
+            print(img_path)
             cnt += 1
             img = cv2.imread(img_path,1)
             img = img[...,::-1]
             ax = plt.subplot(figs[0], figs[1], cnt)
-            ax.set_xlabel('fp:{}  anchor:{} '.format(r,c))
+            ax.set_xlabel(img_path)
             plt.imshow(img)
 
     plt.show()
