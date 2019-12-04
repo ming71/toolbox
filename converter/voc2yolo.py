a46545f6e95c0fd1c07a26ffc1a7a7f44224f2b9
ming71 <mq_chaser@126.com>
2019-12-04 10:48:00 +0800

    support HRSC++ dataset

--- converter/voc2yolo.py
+++ converter/voc2yolo.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import cv2
+import math
 from decimal import Decimal
 
 # 检查异常文件并返回
 # 异常类型：1. xywh数值超出1（图像范围）  2. 负值（max和min标反了的）
-def check_exception(txt_path):
+def check_exception(txt_path,rotate=False):
    files = os.listdir(txt_path)
    class_id = []
    exception = []
@@ -16,14 +17,25 @@ def check_exception(txt_path):
          lines = [i for i in lines if len(i)>0]
          for line in lines:
             line = line.split(' ')
-            assert len(line) == 5 ,'wrong length!!'
-            c,x,y,w,h = line
-            if c not in class_id:
-               class_id.append(c)
-            if float(x)>1.0 or float(y)>1.0 or float(w)>1.0 or float(h)>1.0:
-               exception.append(file)
-            elif float(x)<0 or float(y)<0 or float(w)<0 or float(h)<0:
-               exception.append(file)
+            if not rotate:
+               assert len(line) == 5 ,'wrong length!!'
+               c,x,y,w,h = line
+               if c not in class_id:
+                  class_id.append(c)
+               if float(x)>1.0 or float(y)>1.0 or float(w)>1.0 or float(h)>1.0:
+                  exception.append(file)
+               elif float(x)<0 or float(y)<0 or float(w)<0 or float(h)<0:
+                  exception.append(file)
+            else: 
+               assert len(line) == 6 ,'wrong length!!'
+               c,x,y,w,h,a = line
+               if c not in class_id:
+                  class_id.append(c)
+               if float(x)>1.0 or float(y)>1.0 or float(w)>1.0 or float(h)>1.0 or (float(eval(a))>0.5*math.pi or float(eval(a))<-0.5*math.pi):
+                  exception.append(file)
+               elif float(x)<0 or float(y)<0 or float(w)<0 or float(h)<0:
+                  exception.append(file)
+            
    assert '0' in class_id , 'Class counting from 0 rather than 1!'
    if len(exception) ==0:
       return 'No exception found.'
@@ -61,13 +73,56 @@ def convert(xml_path,txt_path,class_names):
 
                   f.write(str(class_label)+' '+str(x)+' '+str(y)+' '+str(w)+' '+str(h)+'\n')
 
+def convert_HRSCplus(xml_path,txt_path,class_names,rotate=True):
+   xml_files= os.listdir(xml_path)                            
+   for xml_file in xml_files:                                      #每个文件名称
+      with open(os.path.join(txt_path,os.path.splitext(xml_file)[0]+'.txt'),'w') as f:   #打开要写的文件
+         with open(os.path.join(xml_path,xml_file),'r') as fd:        #打开要读的文件
+               contents=fd.read()
+               objects=contents.split('<object>')
+               assert len(objects) > 0, 'No object found in ' + xml_path 
+               
+               info = objects.pop(0)
+               width  = int(info[info.find('<width>')+7 : info.find('</width>')])
+               height = int(info[info.find('<height>')+8 : info.find('</height>')])
+
+               for object in objects:
+                  # 提取类别id (class根据需要自定义)
+                  class_name = object[object.find('<name>')+6 : object.find('</name>')]
+                  class_label = 0
+                  # 提取坐标
+                  if not rotate:
+                     xmin = int(object[object.find('<xmin>')+6 : object.find('</xmin>')])
+                     xmax = int(object[object.find('<xmax>')+6 : object.find('</xmax>')])
+                     ymin = int(object[object.find('<ymin>')+6 : object.find('</ymin>')])
+                     ymax = int(object[object.find('<ymax>')+6 : object.find('</ymax>')])
+                     x = Decimal(0.5*(xmax+xmin)/width).quantize(Decimal('0.000000'))
+                     y = Decimal(0.5*(ymax+ymin)/height).quantize(Decimal('0.000000'))
+                     w = Decimal((xmax-xmin)/width).quantize(Decimal('0.000000'))
+                     h = Decimal((ymax-ymin)/height).quantize(Decimal('0.000000'))
+                     f.write(str(class_label)+' '+str(x)+' '+str(y)+' '+str(w)+' '+str(h)+'\n')
+
+                  else:
+                     cx = float(object[object.find('<rbox_cx>')+9 : object.find('</rbox_cx>')])
+                     cy = float(object[object.find('<rbox_cy>')+9 : object.find('</rbox_cy>')])
+                     w  = float(object[object.find('<rbox_w>')+8 : object.find('</rbox_w>')])
+                     h  = float(object[object.find('<rbox_h>')+8 : object.find('</rbox_h>')])
+                     a  = object[object.find('<rbox_ang>')+10 : object.find('</rbox_ang>')]
+                     cx = Decimal(cx/width).quantize(Decimal('0.000000'))
+                     cy = Decimal(cy/height).quantize(Decimal('0.000000'))
+                     w = Decimal(w/width).quantize(Decimal('0.000000'))
+                     h = Decimal(h/height).quantize(Decimal('0.000000'))
+                     a = Decimal(a).quantize(Decimal('0.000000'))
+                     f.write(str(class_label)+' '+str(cx)+' '+str(cy)+' '+str(w)+' '+str(h)+' '+str(a)+'\n')
+
 
 if __name__ == "__main__":
-   xml_path = r"/py/datasets/ship/tiny_ships/yolo_ship/train_labels"
-   txt_path =  r"/py/datasets/ship/tiny_ships/yolo_ship/val_yolo_anno"
+   xml_path = r"/py/datasets/HRSC+/partition/train_label"
+   txt_path =  r"/py/datasets/HRSC+/partition/train_yolo"
 
-   class_names = ['carrier','cruiser','ship']  # 从0开始编号
+   class_names = ['ship']  # 从0开始编号
 
-   # convert(xml_path,txt_path,class_names)
-   exception_files = check_exception(txt_path)
+   convert_HRSCplus(xml_path,txt_path,class_names,rotate=True)
+   
+   exception_files = check_exception(txt_path,rotate=True)
    print(exception_files)
--- drawbox/drawbox.py
+++ drawbox/drawbox.py
@@ -14,6 +14,46 @@ import math
 # return的四个点顺时针逆时针都可以，但是别调对角线
 
 
+def get_HRSCplus_points(label_path,rotate=False):
+    with open(label_path,'r') as f:        
+        contents=f.read()
+        objects=contents.split('<object>')	
+        objects.pop(0)
+
+        object_coors=[]	# coor内一个元素是一个物体，含四个点8坐标
+        for object in objects:
+            if not rotate:
+                xmin = object[object.find('<xmin>')+10 : object.find('</xmin>')]
+                ymin = object[object.find('<ymin>')+10 : object.find('</ymin>')]
+                xmax = object[object.find('<xmax>')+10 : object.find('</xmax>')]
+                ymax = object[object.find('<ymax>')+10 : object.find('</ymax>')]
+                x0=xmin; y0=ymin; x1=xmin; y1=ymax; x2=xmax; y2=ymax; x3=xmax; y3=ymin
+            else:   # 旋转框
+                cx = float(object[object.find('<rbox_cx>')+9 : object.find('</rbox_cx>')])
+                cy = float(object[object.find('<rbox_cy>')+9 : object.find('</rbox_cy>')])
+                w  = float(object[object.find('<rbox_w>')+8 : object.find('</rbox_w>')])
+                h  = float(object[object.find('<rbox_h>')+8 : object.find('</rbox_h>')])
+                a  = object[object.find('<rbox_ang>')+10 : object.find('</rbox_ang>')]
+
+                xmin = cx - w*0.5; xmax = cx + w*0.5; ymin = cy - h*0.5; ymax = cy + h*0.5
+                t_x0=xmin; t_y0=ymin; t_x1=xmin; t_y1=ymax; t_x2=xmax; t_y2=ymax; t_x3=xmax; t_y3=ymin
+                a = float(a) if not a[0]=='-' else -float(a[1:])
+                # print(-a*180/math.pi)
+                R = np.eye(3)
+                R[:2] = cv2.getRotationMatrix2D(angle=-a*180/math.pi, center=(cx,cy), scale=1)
+                x0 = t_x0*R[0,0] + t_y0*R[0,1] + R[0,2] 
+                y0 = t_x0*R[1,0] + t_y0*R[1,1] + R[1,2] 
+                x1 = t_x1*R[0,0] + t_y1*R[0,1] + R[0,2] 
+                y1 = t_x1*R[1,0] + t_y1*R[1,1] + R[1,2] 
+                x2 = t_x2*R[0,0] + t_y2*R[0,1] + R[0,2] 
+                y2 = t_x2*R[1,0] + t_y2*R[1,1] + R[1,2] 
+                x3 = t_x3*R[0,0] + t_y3*R[0,1] + R[0,2] 
+                y3 = t_x3*R[1,0] + t_y3*R[1,1] + R[1,2] 
+            object_coors.append(np.array([x0,y0,x1,y1,x2,y2,x3,y3]).reshape(4,2).astype(np.int32))
+    return object_coors 
+
+
+
 # for xml style(四点八坐标)
 def get_xml_points(label_path):
     with open(label_path,'r') as f:        
@@ -36,7 +76,7 @@ def get_xml_points(label_path):
 
 
 # for HRSC style(xml标注)
-# 两种模式：rect是两点的xyxy格式； rotate是xywha(其中a顺时针为正，+-0.5pi)
+# 两种模式：rect是两点的xyxy格式； rotate是xywha(其中a顺时针为正，+-0.5pi,x+为0)
 def get_HRSC_points(label_path,rotate=False):
     with open(label_path,'r') as f:        
         contents=f.read()
@@ -80,7 +120,6 @@ def get_HRSC_points(label_path,rotate=False):
 def get_DOTA_points(label_path):
     with open(label_path,'r') as f:        
         contents=f.read()
-        # ipdb.set_trace()
         lines=contents.split('\n')
         lines = [x for x in contents.split('\n')  if x]	 # 移除空格
 
@@ -121,6 +160,7 @@ def get_yolo_points(img_path, rotate=False):
                     x2 = x+0.5*w; y2 = y+0.5*h; x3 = x-0.5*w; y3 = y+0.5*h
                 else:       ## 旋转
                     a = coors[5]
+                    print([x,y,w,h,a])
                     xmin = x - w*0.5; xmax = x + w*0.5; ymin = y - h*0.5; ymax = y + h*0.5
                     t_x0=xmin; t_y0=ymin; t_x1=xmin; t_y1=ymax; t_x2=xmax; t_y2=ymax; t_x3=xmax; t_y3=ymin
                     a = float(a) if not a[0]=='-' else -float(a[1:])
@@ -141,6 +181,7 @@ def get_yolo_points(img_path, rotate=False):
 
 
 # 输入：单张图片和其标注xml提取的所有物体坐标的list,save_flag
+# coor格式：array([[ 43, 436],[103, 496],[472, 127],[412,  67]]
 def drawbox(img_path,object_coors,save_flag=False,save_path=None):
     print(img_path)
     img=cv2.imread(img_path,1)
@@ -149,15 +190,23 @@ def drawbox(img_path,object_coors,save_flag=False,save_path=None):
         if save_flag:
         	cv2.imwrite(os.path.join(save_path,os.path.split(img_path)[1]), img)
         else: 
-            cv2.imshow('drawbox',img)
+            cv2.imshow(img_path,img)
+            cv2.moveWindow(img_path,100,100)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
-    img_path = '/py/datasets/ship/tiny_ships/yolo_ship/single_class/train_imgs'
-    label_path = '/py/datasets/ship/tiny_ships/yolo_ship/single_class/train_imgs'
-    save_path = '/py/datasets/ship/tiny_ships/yolo_ship/single_class/train_imgs'
-
+    # img_path = '/py/datasets/HRSC2016/yolo-dataset/train'
+    # label_path = '/py/datasets/HRSC2016/yolo-dataset/train'
+    # save_path = '/py/datasets/ship/tiny_ships/yolo_ship/single_class/train_imgs'
+    
+    # 注意：如果是yolo，img和label放到一个文件夹下，并且路径设置也要一样
+    img_path = '/py/datasets/HRSC+/partition/val'
+    label_path = '/py/datasets/HRSC+/partition/val'
+    func = get_yolo_points
+    
     
     # for folder
     if os.path.isdir(img_path) and os.path.isdir(label_path):
@@ -169,15 +218,18 @@ if __name__ == "__main__":
         for iter in iterations:
             assert os.path.splitext(iter[0])[0]==os.path.splitext(iter[1])[0],'图像和label不匹配！'
             # 选择label的模式
-            object_coors = get_yolo_points(os.path.join(label_path,iter[1]), rotate=False)
-            if len(object_coors):
-                drawbox(os.path.join(img_path,iter[0]),object_coors,False,save_path=save_path)
-            else:
-                print('No obj!')
+            # object_coors = get_yolo_points(os.path.join(label_path,iter[1]), rotate=True)
+            if not iter[0].endswith('.txt'):
+                object_coors = func(os.path.join(label_path,iter[1]), rotate=True)
+                if len(object_coors):
+                    drawbox(os.path.join(img_path,iter[0]),object_coors,False)
+                else:
+                    print('No obj!')
+    
     # for single img
     elif os.path.isfile(label_path):
-        # pass
-        object_coors = get_DOTA_points(label_path)
-        drawbox(img_path,object_coors,False)
+        object_coors = func(os.path.join(label_path), rotate=True)
+        if len(object_coors):
+            drawbox(img_path,object_coors,False)
     else:
         print('Path Not Matched!!!')
