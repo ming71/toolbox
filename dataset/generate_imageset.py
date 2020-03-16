9873e9fdff396dd4039180671b23b5ca80556867
“ming71” <mq_chaser@126.com>
2020-03-16 17:44:28 +0800

    modify imageset generation file

--- dataset/generate_imageset.py
+++ dataset/generate_imageset.py
@@ -4,21 +4,14 @@ import sys
 
 
 ##--------------- 分类标注 -----------------
-img_path = r"/py/datasets/ship/tiny_ships/yolo_ship/val_imgs" 
-set_file = r'/py/datasets/ship/tiny_ships/yolo_ship/val.txt'
+img_path = r"/py/BoxesCascade/HRSC2016/Train/AllImages" 
+set_file = r'/py/BoxesCascade/HRSC2016/trainval.txt'
 
-mode = 'yolo'
 
 files= os.listdir(img_path) #得到文件夹下的所有文件名称
+with open(set_file,'w') as f:
+	for file in files:
+		filename, extension = os.path.splitext(file)
+		if extension in ['.jpg', '.bmp']:
+			f.write(os.path.join(img_path,file)+'\n')
 
-if mode == 'voc':
-	with open(set_file,'w') as f:
-		for file in files:
-			f.write(file[:-4]+'\n')
-
-if mode == 'yolo':
-	with open(set_file,'w') as f:
-		for file in files:
-			filename, extension = os.path.splitext(file)
-			if extension == '.jpg':
-				f.write(os.path.join(img_path,file)+'\n')
