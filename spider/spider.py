e42d11532375d24267e27e123cfcd767a285ab3a
ming71 <43720092+ming71@users.noreply.github.com>
2020-05-19 12:14:36 +0800

    support strokes_query.

--- spider/spider.py
+++ spider/spider.py
@@ -1,12 +1,11 @@
 '''
 2019.10.24
-功能： 自动根据提供内容进行反复搜索查询，省去手工多次输入和记录的麻烦
-（花了十来分钟帮女票弄得）
-
-Bug Report:
-    没有解决windows的txt在linux下读取bug的程序问题。
-    将文本的内容复制到网页上，然后粘贴到linux下新建的txt空白文本即可。
-    不同笔记本的文档建立格式不同同理，可以更改提供的coding方式，再不行就复制粘贴，断点调试words输出可识别即可
+功能： 查询词频和笔画数
+实现： 非常低级的爬虫和字符串匹配
+问题： 
+    1. 编码确认无误。windows下由于源文件不同，txt经常会出现乱码，需要选择合适的encode方式，先查看读取是否有误再运行
+    2. 字符串匹配bug。往往是多次匹配或者提前匹配。
+    3. 不同操作系统的txt显示乱码。同1，嫌麻烦可以直接复制到别的地方然后粘贴。
 '''
 import requests
 from urllib.parse import quote
@@ -15,10 +14,7 @@ import glob
 from tqdm import tqdm
 import sys
 
-
-if __name__ == "__main__":
-    src_path = '/py/toolbox/spider/search'
-
+def frequence_query(src_path):
     # 文件夹输入，将多输入和检索结果丢在一起比较方便
     if os.path.isdir(src_path):     
         files = sorted(glob.glob(os.path.join(src_path, '*.*')))
@@ -30,21 +26,69 @@ if __name__ == "__main__":
                 os.remove(save_path)
                 files.pop(files.index(save_path))
 
-            with open(path,'r') as f:   # encoding='utf-8'，'gbk'
+            # with open(path,'r', encoding='utf-8',errors='ignore') as f:
+            with open(path,'r', encoding='gbk',errors='ignore') as f:
                 with open(save_path,'a') as fw:
                     contents = f.readlines()
-                    words = [word.strip('\n').strip(' ') for word in contents]
+                    import ipdb; ipdb.set_trace()
+                    words = [word.strip('\n').strip('') for word in contents]
                     assert len(contents)==len(words), '有空行，检查一下是不是输入有问题'
 
                     for word in tqdm(words):
                         urlencode = quote(word)
                         url='http://bcc.blcu.edu.cn/zh/search/2/' + urlencode
-                        req = requests.get(url)  
-                        import ipdb; ipdb.set_trace()
+                        req = requests.get(url) 
                         result=req.text[req.text.index('\t    共')+6:req.text.index('个结果')].strip(' ') # 搜索目标结果
+                        print(word)
+                        # import ipdb; ipdb.set_trace()
                         fw.write(result+'\n')
 
     elif os.path.isfile(src_path):
         print('将文件放到对应的文件夹下即可')
     else:
         print('路径不对，检查一下')
+
+
+## txt文件内所有汉字无视词语组合直接输出成单列
+def strokes_query(src_path):
+    # 文件夹输入，将多输入和检索结果丢在一起比较方便
+    if os.path.isdir(src_path):     
+        files = sorted(glob.glob(os.path.join(src_path, '*.*')))
+        for path in files:
+            file_name = os.path.split(path)[1]
+            save_path = os.path.join(src_path,os.path.splitext(file_name)[0]+'_result.txt')
+
+            if os.path.exists(save_path):
+                os.remove(save_path)
+                files.pop(files.index(save_path))
+
+            # with open(path,'r', encoding='utf-8',errors='ignore') as f:
+            with open(path,'r', encoding='gbk',errors='ignore') as f:
+                with open(save_path,'a') as fw:
+                    contents = f.read()
+                    words = contents.replace('\n','').strip('')
+
+                    for word in tqdm(words):
+                        url='http://bishun.strokeorder.info/mandarin.php?q=' + word
+                        req = requests.get(url) 
+                        content = req.content.decode('utf-8')
+                        result = content[content.index('的笔画数')+9 : content.index('<br><br>\n\n<b>相关汉字的笔顺')]
+                        # print(word)
+                        # import ipdb; ipdb.set_trace()
+                        fw.write(result+'\n')
+
+    elif os.path.isfile(src_path):
+        print('将文件放到对应的文件夹下即可')
+    else:
+        print('路径不对，检查一下')
+
+
+
+if __name__ == "__main__":
+    src_path = 'D:\\application\Jupyter Notebook\spider\search'
+
+    # frequence_query(src_path)
+    strokes_query(src_path)
+
+
+
