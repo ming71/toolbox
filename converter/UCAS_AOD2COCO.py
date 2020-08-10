7a1c024dcb4f65dbdcf5d2e92bb5b3f0e1e8723a
ming71 <chaser.ming@gmail.com>
2020-08-10 17:26:49 +0800

    add UCAS_AOD2COCO

--- converter/UCAS_AOD2COCO.py
+++ converter/UCAS_AOD2COCO.py
@@ -1,28 +1,29 @@
-import os
-import cv2
-import glob
-import json
-import sys
-from PIL import Image
-
-import numpy as np
 import dota_utils as util
+import os
+import sys
+import cv2
+import math
+import json
+import numpy as np
 from utils import *
+
+from PIL import Image
 from tqdm import tqdm
 
-class_name = ['text']
 
-def ICDAR2COCOTrain(srcpath, destfile, cls_names):
-    imageparent = os.path.join(srcpath, 'images')
-    labelparent = os.path.join(srcpath, 'labels')
+classnames = ['car', 'airplane']
+
+def UCAS_AOD2COCOTrain(srcpath, destfile, cls_names, train_set_file):
+    imageparent = os.path.join(srcpath, 'AllImages')
+    labelparent = os.path.join(srcpath, 'Annotations')
 
     data_dict = {}
     info = { 'contributor': 'ming71',
              'data_created': '2020',
-             'description': 'ICDAR15 dataset',
+             'description': 'UCAS_AOD',
              'url': 'sss',
              'version': '1.0',
-             'year': 2020}
+             'year': 2014}
     data_dict['info'] = info
     data_dict['images'] = []
     data_dict['categories'] = []
@@ -33,29 +34,30 @@ def ICDAR2COCOTrain(srcpath, destfile, cls_names):
 
     inst_count = 1
     image_id = 1
+    with open(train_set_file, 'r') as f_set:
+        filenames = [x.strip('\n') for x in f_set.readlines()]
+        # import ipdb;ipdb.set_trace()
+
     with open(destfile, 'w') as f_out:
         # filenames = util.GetFileFromThisRootDir(labelparent)
-        filenames = glob.glob(labelparent + '/*.txt')
-
-        for file in tqdm(filenames):
-            basename = util.custombasename(file)
-            # image_id = int(basename[1:])
-
-            imagepath = os.path.join(imageparent, basename + '.jpg')
+        pbar = tqdm(filenames)
+        for filename in pbar:
+            pbar.set_description("UCAS_AOD2COCOTrain")
+            imagepath = os.path.join(imageparent, filename + '.png')
 
             img = Image.open(imagepath)
             height = img.height
             width = img.width
 
             single_image = {}
-            single_image['file_name'] = basename + '.jpg'
+            single_image['file_name'] = filename + '.png'
             single_image['id'] = image_id
             single_image['width'] = width
             single_image['height'] = height
             data_dict['images'].append(single_image)
 
             # annotations
-            objects = parse_icdar_poly2(file)
+            objects = parse_txt_poly2(os.path.join(labelparent, filename+'.txt'))
             for obj in objects:
                 single_obj = {}
                 single_obj['area'] = obj['area']
@@ -75,35 +77,34 @@ def ICDAR2COCOTrain(srcpath, destfile, cls_names):
             image_id = image_id + 1
         json.dump(data_dict, f_out)
 
-def ICDAR2COCOTest(srcpath, destfile, cls_names):
-    imageparent = os.path.join(srcpath, 'images')
-    # labelparent = os.path.join(srcpath, 'labelTxt')
+def UCAS_AOD2COCOTest(srcpath, destfile, cls_names, test_set_file):
+    imageparent = os.path.join(srcpath, 'AllImages')
     data_dict = {}
     info = { 'contributor': 'ming71',
              'data_created': '2020',
-             'description': 'ICDAR15 dataset',
+             'description': 'UCAS_AOD',
              'url': 'sss',
              'version': '1.0',
-             'year': 2020}
+             'year': 2014}
     data_dict['info'] = info
     data_dict['images'] = []
     data_dict['categories'] = []
     for idex, name in enumerate(cls_names):
         single_cat = {'id': idex + 1, 'name': name, 'supercategory': name}
         data_dict['categories'].append(single_cat)
+    
+    with open(test_set_file, 'r') as f_set:
+        filenames = [x.strip('\n') for x in f_set.readlines()]
 
     inst_count = 1
     image_id = 1
     with open(destfile, 'w') as f_out:
         # filenames = util.GetFileFromThisRootDir(imageparent)
-        filenames = glob.glob(imageparent + '/*.jpg')
-
-
-        for file in tqdm(filenames):
-            basename = util.custombasename(file)
+        pbar = tqdm(filenames)
+        for filename in pbar:
+            pbar.set_description("UCAS_AOD2COCOTest")
             # image_id = int(basename[1:])
-
-            imagepath = os.path.join(imageparent, basename + '.jpg')
+            imagepath = os.path.join(imageparent, filename + '.png')
             # img = cv2.imread(imagepath)
             img = Image.open(imagepath)
             # height, width, c = img.shape
@@ -111,7 +112,7 @@ def ICDAR2COCOTest(srcpath, destfile, cls_names):
             width = img.width
 
             single_image = {}
-            single_image['file_name'] = basename + '.jpg'
+            single_image['file_name'] = filename + '.png'
             single_image['id'] = image_id
             single_image['width'] = width
             single_image['height'] = height
@@ -123,7 +124,7 @@ def ICDAR2COCOTest(srcpath, destfile, cls_names):
 
 
 
-def parse_icdar_poly(filename):
+def parse_txt_poly(filename):
     objects = []
     #print('filename:', filename)
     f = []
@@ -140,14 +141,14 @@ def parse_icdar_poly(filename):
         # if count < 2:
         #     continue
         if line:
-            splitlines = line.strip().split(',')
+            splitlines = line.strip().split()
             object_struct = {}
-            object_struct['name'] = 'text'
-            object_struct['difficult'] = '0' if splitlines[-1] != '###' else '1'
-            object_struct['poly'] = [(float(splitlines[0]), float(splitlines[1])),
-                                     (float(splitlines[2]), float(splitlines[3])),
-                                     (float(splitlines[4]), float(splitlines[5])),
-                                     (float(splitlines[6]), float(splitlines[7]))
+            object_struct['name'] = splitlines[0]
+            object_struct['difficult'] = '0' 
+            object_struct['poly'] = [(float(splitlines[1]), float(splitlines[2])),
+                                     (float(splitlines[3]), float(splitlines[4])),
+                                     (float(splitlines[5]), float(splitlines[6])),
+                                     (float(splitlines[7]), float(splitlines[8]))
                                      ]
             gtpoly = shgeo.Polygon(object_struct['poly'])
             object_struct['area'] = gtpoly.area
@@ -163,23 +164,28 @@ def parse_icdar_poly(filename):
             break
     return objects
 
-def parse_icdar_poly2(filename):
-    objects = parse_icdar_poly(filename)
+def parse_txt_poly2(filename):
+    objects = parse_txt_poly(filename)
     for obj in objects:
         obj['poly'] = TuplePoly2Poly(obj['poly'])
         obj['poly'] = list(map(int, obj['poly']))
     return objects
 
-
 if __name__ == '__main__':
 
-    ICDAR2COCOTrain(r'/data-input/AerialDetection-master/data/ICDAR15/train',
-                   r'/data-input/AerialDetection-master/data/ICDAR15/train/IC15.json',
-                   class_name)
-    ICDAR2COCOTest(r'/data-input/AerialDetection-master/data/ICDAR15/val',
-                  r'/data-input/AerialDetection-master/data/ICDAR15/val/IC15.json',
-                  class_name)
-
+    UCAS_AOD2COCOTrain(r'/data-input/AerialDetection-master/data/UCAS_AOD',
+                   r'/data-input/AerialDetection-master/data/UCAS_AOD/train.json',
+                   classnames,
+                   r'/data-input/AerialDetection-master/data/UCAS_AOD/ImageSets/train.txt')
+    UCAS_AOD2COCOTrain(r'/data-input/AerialDetection-master/data/UCAS_AOD',
+                   r'/data-input/AerialDetection-master/data/UCAS_AOD/val.json',
+                   classnames,
+                   r'/data-input/AerialDetection-master/data/UCAS_AOD/ImageSets/val.txt')
+   
+    UCAS_AOD2COCOTest(r'/data-input/AerialDetection-master/data/UCAS_AOD',
+                  r'/data-input/AerialDetection-master/data/UCAS_AOD/test.json',
+                  classnames,
+                  r'/data-input/AerialDetection-master/data/UCAS_AOD/ImageSets/test.txt')
 
 
 
