2ee12c85fdc65e67f068271946e2626c00dff481
ming71 <chaser.ming@gmail.com>
2020-08-27 11:30:45 +0800

    add fonts for seborn plot

--- matplotlib/seborn.py
+++ matplotlib/seborn.py
@@ -10,12 +10,13 @@ x = bf_ious[valid_idx]
 y = scores[valid_idx]
 # 设置风格
 sns.set_context("paper")
+sns.set(font_scale=1.7)	# 坐标轴字体
 sns.set_style("white") 
 # kind有三种方式reg--绘制拟合直线；hex--绘制蜂窝状散点图；kde--绘制核密度图
 # ratio越大，边栏越窄
 df = pd.DataFrame(np.stack((x,y),-1),columns=["iou", "conf"])
 g = sns.jointplot(x="iou", y="conf", data=df, kind="kde",space=0, ratio=10, height=9)   # color=xxx
-g.set_axis_labels("output IoU","Classification Confidence",fontsize=16)
+g.set_axis_labels("output IoU","Classification Confidence",fontsize=30,fontfamily='Times New Roman')
 # g.plot_joint(plt.scatter, c="r", s=5, linewidth=1, marker=".")     # 把散点图叠加画到高斯图上去
 # sns.despine()
 g.savefig(base_partial_save_path, dpi = 600)
