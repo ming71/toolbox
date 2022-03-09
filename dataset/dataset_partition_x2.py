fd37a821b80f0fc5dc91badc9c1f7860173f4785
ming71 <mq_chaser@126.com>
2022-03-09 11:17:07 +0800

    fix a coding bug

--- dataset/dataset_partition_x2.py
+++ dataset/dataset_partition_x2.py
@@ -1,17 +1,19 @@
-# date：   2018.12.17 
-# update： 2019.9.27
-# author:  ming71
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
 import os 
 import sys
 import random
 import ipdb
 import shutil
-# random.seed(666)
+from tqdm import tqdm
+random.seed(666)
 
 
 def clear_folder(path):
-    if os.listdir(path) == []:
+    if not os.path.exists(path):
+        os.makedirs(path)
+    elif os.listdir(path) == []:
         print('{} is already clean'.format(path))
     else:
         files = os.listdir(path)
@@ -23,7 +25,7 @@ def clear_folder(path):
 def division_and_copy(src_path,dst_path,indexes):
     files= os.listdir(src_path) 
     files.sort()        # !!!!排序，不然label就乱了   
-    for index in  indexes:
+    for index in tqdm(indexes):
         src = os.path.join(src_path,files[index])
         dst = os.path.join(dst_path,files[index])
         shutil.copyfile(src,dst)
@@ -33,14 +35,14 @@ def division_and_copy(src_path,dst_path,indexes):
 if __name__ == "__main__":
         
     # Setting
-    train_ratio      = 0.8     # 数据集不大时，验证集测试集多拿一点
+    train_ratio      = 0.8    
     val_ratio        = 0.2
-    src_imgs         = r'/py/datasets/HRSC+/FullDataset/img'
-    src_labels       = r'/py/datasets/HRSC+/FullDataset/label'
-    dst_train_imgs   = r'/py/datasets/HRSC+/train'
-    dst_train_labels = r'/py/datasets/HRSC+/train_label'
-    dst_val_imgs     = r'/py/datasets/HRSC+/val'
-    dst_val_labels   = r'/py/datasets/HRSC+/val_label'    
+    src_imgs         = r'total_dataset/images'
+    src_labels       = r'total_dataset/labelTxt'
+    dst_train_imgs   = r'train/images'
+    dst_train_labels = r'train/labelTxt'
+    dst_val_imgs     = r'val/images'
+    dst_val_labels   = r'val/labelTxt'  
 
     total_size = len(os.listdir(src_imgs))  # 原始数据集大小
 
@@ -55,7 +57,11 @@ if __name__ == "__main__":
     clear_folder(dst_val_imgs)
     clear_folder(dst_val_labels)
 
+    print('copying train imgs...')
     division_and_copy(src_imgs,dst_train_imgs,train_index)
+    print('copying val imgs...')
     division_and_copy(src_imgs,dst_val_imgs,val_index)
+    print('copying train labels...')
     division_and_copy(src_labels,dst_train_labels,train_index)
+    print('copying val labels...')
     division_and_copy(src_labels,dst_val_labels,val_index)
